3e61e412dea0cc00086f620322b9bc17c5697b52
SeongWon <59052363+seongwonyang@users.noreply.github.com>
2023-08-03 17:37:03 +0900

    Update SecurityConfiguration.java

--- for-model/gateway/src/main/java/com/example/template/config/SecurityConfiguration.java
+++ for-model/gateway/src/main/java/com/example/template/config/SecurityConfiguration.java
@@ -1,6 +1,6 @@
 path: /src/main/java/{{options.package}}/config
 ---
-package {{options.package}}/config;
+package {{options.package}}.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
