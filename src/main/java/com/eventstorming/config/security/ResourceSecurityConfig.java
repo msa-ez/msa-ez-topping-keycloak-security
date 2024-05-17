a3dbf2384a9bdce1cedebaab6a1c19422fa24343
SeongWon <59052363+seongwonyang@users.noreply.github.com>
2024-05-17 12:29:13 +0900

    Update ResourceSecurityConfig.java

--- src/main/java/com/eventstorming/config/security/ResourceSecurityConfig.java
+++ src/main/java/com/eventstorming/config/security/ResourceSecurityConfig.java
@@ -14,6 +14,10 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+{{#if (isSelectedEgovDefault options.rootModel.toppingPlatforms)}}
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+{{/if}}
 
 import java.util.Collection;
 import java.util.Map;
@@ -26,7 +30,30 @@ public class ResourceSecurityConfig  {
 
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		{{#if (isSelectedEgovDefault options.rootModel.toppingPlatforms)}}
+		// 커스터마이징한 RequestMatcher 생성
+        	RequestMatcher publicUrlsWithoutAuthHeader = request -> 
+	            	new AntPathRequestMatcher("here is api url").matches(request) && 
+	            	request.getHeader("Authorization") == null;
+		{{/if}}
+		
 		 http
+			{{#if (isSelectedEgovDefault options.rootModel.toppingPlatforms)}}
+			.authorizeRequests()
+			// 커스터마이징한 RequestMatcher를 사용하여 조건부 인증 설정
+			.requestMatchers(publicUrlsWithoutAuthHeader).permitAll()
+			// 다른 모든 요청에 대해서는 인증을 요구함
+			.anyRequest().authenticated()
+			.and()
+			.cors()
+			.and()
+			.csrf()
+			.disable()
+			.oauth2ResourceServer()
+			.jwt(jwt ->
+				jwt.jwtAuthenticationConverter(grantedAuthoritiesExtractor())
+			);
+			{{else}}
 			.cors().and()
 			.csrf().disable()
 			.authorizeRequests(exchange -> exchange
@@ -37,6 +64,7 @@ public class ResourceSecurityConfig  {
 			)
 			.oauth2ResourceServer()
 			.jwt(jwt -> jwt.jwtAuthenticationConverter(grantedAuthoritiesExtractor()));
+			{{/if}}
 
 		return http.build();
 	}
@@ -59,3 +87,22 @@ public class ResourceSecurityConfig  {
         }
     }	
 }
+
+
+
+<function>
+window.$HandleBars.registerHelper('isSelectedEgovDefault', function (toppings) {
+    try{
+        var isSelectedEgovDefault = false
+        for(var i=0; i<toppings.length; i++){
+            if(toppings[i].includes('egov-default')){
+                isSelectedEgovDefault =  true;
+            }
+        }
+
+        return isSelectedEgovDefault;
+    } catch(e){
+        console.log(e)
+    }
+});
+</function>
